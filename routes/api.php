46a6b249dd898275a6ddc0781187dc7282af1fc5
Shkretov97@mail.ru <Shkretov97@mail.ru>
2026-05-23 12:03:22 +0500

    fix

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\Admin\ArticleController as AdminArticleController;
 use App\Http\Controllers\AuthController;
 
 Route::post('/login', [AuthController::class, 'login']);
@@ -10,3 +11,6 @@ Route::post('/register', [AuthController::class, 'register']);
 
 Route::get("/articles", [ArticleController::class, "index"]);
 Route::get("/articles/{article}", [ArticleController::class, "show"]);
+
+Route::get('/admin/articles', [AdminArticleController::class, 'index'])->middleware('auth:sanctum');
+Route::delete('/admin/articles/{article}', [AdminArticleController::class, 'delete'])->middleware('auth:sanctum');
