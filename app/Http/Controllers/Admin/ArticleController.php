254b9cd10530b46e60241c740da354837c8b6667
Shkretov97@mail.ru <Shkretov97@mail.ru>
2026-05-24 11:55:49 +0500

    fix

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -8,7 +8,8 @@ use App\Models\Article;
 
 class ArticleController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         return Article::select([
             'id',
             'title',
@@ -16,7 +17,25 @@ class ArticleController extends Controller
         ])->get();
     }
 
-    public function delete(Article $article) {
+    public function store(Request $request)
+    {
+        $data =  $request->validate([
+            'title' => 'required|max:255',
+            'content' => 'required|max:65000',
+            'image' => 'max:255'
+        ]);
+        Article::create([
+            'title' => $data['title'],
+            'content' => $data['content'],
+            'image' => $data['image'],
+        ]);
+        return [
+            'success' => true
+        ];
+    }
+
+    public function delete(Article $article)
+    {
         $article->delete();
         return [
             'success' => true
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
-
-use Illuminate\Http\Request;
+ 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\Admin\ArticleController as AdminArticleController;
@@ -13,4 +12,5 @@ Route::get("/articles", [ArticleController::class, "index"]);
 Route::get("/articles/{article}", [ArticleController::class, "show"]);
 
 Route::get('/admin/articles', [AdminArticleController::class, 'index'])->middleware('auth:sanctum');
+Route::post('/admin/articles', [AdminArticleController::class, 'store'])->middleware('auth:sanctum');
 Route::delete('/admin/articles/{article}', [AdminArticleController::class, 'delete'])->middleware('auth:sanctum');
